c573465e2d2c6015b302f3f6fa634ed116dd24f3
Burockjohn <brkcn87@gmail.com>
2023-07-24 23:06:47 +0300

    BrowserUtils updated

--- src/test/java/com/cydeo/tests/utilities/BrowserUtils.java
+++ src/test/java/com/cydeo/tests/utilities/BrowserUtils.java
@@ -7,8 +7,12 @@ In this class only general utility methods that are not related to some specific
 */
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 
+import java.time.Duration;
 import java.util.Set;
 
 public class BrowserUtils {
@@ -68,16 +72,16 @@ public class BrowserUtils {
 
     }
 
-    /*
+
     public static void waitForInvisibilityOf(WebElement webElement){
         //Creating a utility method for ExplicitWait, so we don't have to repeat the lines
 
         //Driver.getDriver().manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
-        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), 10);
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), Duration.ofSeconds(10));
         wait.until(ExpectedConditions.invisibilityOf(webElement));
     }
 
-     */
+
 
 
 }
